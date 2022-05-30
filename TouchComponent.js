ccad94d0d6840f8c9b29230f6062fa117e70abce
kumar Gaurav <73277183+Kumargaurav11998@users.noreply.github.com>
2022-05-30 11:57:04 +0530

    Add files via upload

--- TouchComponent.js
+++ TouchComponent.js
@@ -1,32 +1,21 @@
-import React from 'react';
-import { Image, TouchableOpacity, Text } from 'react-native';
+import React from "react";
+import { Image, TouchableOpacity, Text } from "react-native";
 
 function TouchComponent(props) {
-  //console.log(props.press)
-  return (
-    <TouchableOpacity
-      onPress={() => props.press()}
-      style={{
-        backgroundColor: props.backgroundColor,
-        borderRadius: props.borderRadius,
-        paddingVertical: props.paddingVertical,
-        paddingHorizontal: props.paddingHorizontal,
-        height: props.height,
-        width: props.width,
-        alignSelf: 'center',
-      }}
-    >
-      <Text
+    //console.log(props.press)
+    return (
+        <TouchableOpacity
+        onPress={()=>props.press()}
         style={{
-          textAlign: 'center',
-          color: props.titlecolor,
-          fontSize: props.fontSize,
-          paddingTop: props.titlepaddingtop,
-        }}
-      >
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  );
+            backgroundColor: props.backgroundColor,
+            borderRadius: props.borderRadius,
+             paddingVertical: props.paddingVertical,
+            paddingHorizontal:props.paddingHorizontal,
+           
+        }}>
+            
+            <Text style={{ textAlign: 'center', color: props.titlecolor , fontSize:props.fontSize}}>{props.title}</Text>
+        </TouchableOpacity>
+    )
 }
-export default TouchComponent;
+export default TouchComponent
